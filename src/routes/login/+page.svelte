696dad4a7179b3489b01bfa00cc76a892a01d3dc
alielchab <ali.elchab@additiv.com>
2026-06-02 14:48:06 +0200

    username and password

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -6,7 +6,11 @@
   <h1>Login</h1>
 
   <p>Nur der Administrator-Login ist vorhanden und berechtigt, sich im System anzumelden.
-    Es gibt keine Möglichkeit, einen neuen Benutzer zu registrieren. Bitte verwenden Sie die folgenden Anmeldedaten:</p>
+    Es gibt keine Möglichkeit, einen neuen Benutzer zu registrieren. Bitte verwenden Sie die folgenden Anmeldedaten:
+  </p>
+  <p>
+    Benutzername <code>admin</code> und Passwort <code>Projekt_2026</code>.
+  </p>
 
   <form method="POST">
     <div class="mb-3">
